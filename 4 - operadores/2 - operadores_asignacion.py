423a54f58ebc35a2aa16ae00e3768912f4411b54
LozanoErnestoCloud <de.gonzalezlozano@outlook.com>
2025-10-19 21:28:13 -0600

    teoria de operadores aritmeticos

---
+++ 4 - operadores/2 - operadores_asignacion.py
@@ -0,0 +1,9 @@
+print('*** OPERADORES DE ASIGNACION ***')
+
+# asignar un numero
+numero = 5
+print(f'La numero es: {numero}')
+
+# asignar una cadena
+cadena = 'hola mundo!'
+print(f'La cadena es: {cadena}')
