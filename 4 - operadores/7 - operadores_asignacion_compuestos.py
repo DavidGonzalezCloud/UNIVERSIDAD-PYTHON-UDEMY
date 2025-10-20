ffad0e7632611ce99e631730e9e163541ad4872a
LozanoErnestoCloud <de.gonzalezlozano@outlook.com>
2025-10-19 22:04:37 -0600

    Intercambio de valores Multiples valores de entrada operadores de asignacion compuestos

---
+++ 4 - operadores/7 - operadores_asignacion_compuestos.py
@@ -0,0 +1,22 @@
+print('**** OPERADORES DE ASIGNACION COMPUESTOS')
+
+a,b = 20,15
+print(f'\nEl valor de a = {a}')
+print(f'El valor de b = {b}')
+
+# operador compuesto de suma +=
+a += b # a = a + b
+print(f'\nEl valor de a = {a}')
+
+# operador compuesto de resta -=
+a = 20
+a -= b # a = a-b
+print(f'\nEl valor de a = {a}')
+
+a = 20
+a *= b # a = a*b
+print(f'\nEl valor de a = {a}')
+
+a = 20
+a /= b # a = a/b
+print(f'\nEl valor de a = {a}')
