c0a4eb68df0a74de98b1e46738273d237d7a77c7
LozanoErnestoCloud <de.gonzalezlozano@outlook.com>
2025-10-19 21:25:21 -0600

    teoria de operadores aritmeticos

---
+++ 4 - operadores/1 - operadores_aritmeticos.py
@@ -0,0 +1,34 @@
+# Operadores aritmeticos
+
+a = 10
+b = 3
+
+# suma
+suma = a + b
+print(f'La suma es: {suma}')
+
+# resta
+resta = a - b
+print(f'La resta es: {resta}')
+
+# multiplicacion
+multiplicacion = a * b
+print(f'La multiplicacion es: {multiplicacion}')
+
+# division
+division = a / b
+print(f'La division es: {division:.2f}')
+
+# division entero
+division_entero = a // b
+print(f'La division entero es: {division_entero}')
+
+# residuo de la division
+residuo = a % b
+print(f'La residuo es: {residuo}')
+
+# exponente
+exponencial = a ** b
+print(f'La exponencial es: {exponencial}')
+
+
