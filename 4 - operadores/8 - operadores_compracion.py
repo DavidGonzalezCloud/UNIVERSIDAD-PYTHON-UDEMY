d9f328898970e7f3c7dcef606042e7ae63352211
LozanoErnestoCloud <de.gonzalezlozano@outlook.com>
2025-10-20 14:00:46 -0600

    Operadores de comparacion

---
+++ 4 - operadores/8 - operadores_compracion.py
@@ -0,0 +1,29 @@
+print('**** OPERADORES DE COMPARACION ****')
+
+a, b = 7,5
+print(f'El valor de a = {a}')
+print(f'El valor de b = {b}')
+
+# Operador de igualdad ==
+resultado = (a==b)
+print(f'El resultado de a==b es {resultado}')
+
+# Operador diferente !=
+resultado = (a!=b)
+print(f'El resultado de a!=b es {resultado}')
+
+# Operador mayor que >
+resultado = (a>b)
+print(f'El resultado de a>b es {resultado}')
+
+# Operador moyor o igual que >=
+resultado = (a>=b)
+print(f'El resultado de a>=b es {resultado}')
+
+# operador de menor que <
+resultado = (a<b)
+print(f'El resultado de a<b es {resultado}')
+
+# operador de manor o igual que <=
+resultado = (a<=b)
+print(f'El resultado de a<=b es {resultado}')
