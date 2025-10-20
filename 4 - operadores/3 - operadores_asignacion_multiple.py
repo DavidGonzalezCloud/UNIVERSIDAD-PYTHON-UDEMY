d7f2bea3e5357778ed781c5b546e522b51d27706
LozanoErnestoCloud <de.gonzalezlozano@outlook.com>
2025-10-19 21:40:00 -0600

    teoria de operadores aritmeticos

---
+++ 4 - operadores/3 - operadores_asignacion_multiple.py
@@ -0,0 +1,8 @@
+print('*** OPERADORES DE ASIGNACION MULTIPLE ***')
+
+# SINTAXIS DE ASIGNACION MULTIPLE
+a,b = 10,3
+print(f'El valor de a es {a} y el valor de b es {b}')
+
+a,b,c = 10,'hola mundo',5.24
+print(f'El valor de a es {a} y el valor de b es {b} y el valor de c es {c}')
---
+++ 4 - operadores/4 - operadores_asignacion_encadenada.py
@@ -0,0 +1,16 @@
+print('**** Operadores de asignacion encadenada ****')
+
+contador1 = contador2 = contador3 = contador4 = contador5 = 0
+print(f'El valor de contador 1 es {contador1}')
+print(f'El valor de contador 2 es {contador2}')
+print(f'El valor de contador 3 es {contador3}')
+print(f'El valor de contador 4 es {contador4}')
+print(f'El valor de contador 5 es {contador5}')
+
+contador1 = contador2 = contador3 = contador4 = contador5 = 1
+
+print(f'\nEl valor de contador 1 es {contador1}')
+print(f'El valor de contador 2 es {contador2}')
+print(f'El valor de contador 3 es {contador3}')
+print(f'El valor de contador 4 es {contador4}')
+print(f'El valor de contador 5 es {contador5}')
