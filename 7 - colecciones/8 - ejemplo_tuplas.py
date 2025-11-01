53f548817cf06c1cf342b848919676adb3ea5406
LozanoErnestoCloud <de.gonzalezlozano@outlook.com>
2025-11-01 16:32:05 -0600

    Operadores de comparacion

---
+++ 7 - colecciones/8 - ejemplo_tuplas.py
@@ -0,0 +1,7 @@
+# EJEMPLOS DE TUPLAS
+
+tupla_numeros = (1,2,3,4,5)
+tupla_mixta = ('manzana', 10, 3.14, [1,2,3])
+tupla_sin_parentesis = 'juan', 'karla'
+tupla_un_elemento = 10,
+
---
+++ 7 - colecciones/9 - tuplas.py
@@ -0,0 +1,14 @@
+print('***** MANEJO DE TUPLAS *****')
+
+mi_tupla = (1,2,3,4,5)
+print(mi_tupla)
+
+# CREAMOS UNA TUPLA PARA UNA COORDENADA X,Y
+coordenadas = (3,5)
+# ACCEDEMOS A CADA ELEMENTO DE LA TUPLA
+print(f'\nCoordenada en el eje x: {coordenadas[0]} y y: {coordenadas[1]}')
+
+# Tupla Anidada
+tupla_anidada = (1, (2,3), (4,5))
+print(f'Tupla anidada: {tupla_anidada}')
+print(f"Indice 1 de la tupla es: {tupla_anidada[1]}")
